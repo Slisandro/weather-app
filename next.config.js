59b3a65133843b51faf6fe73cbfd0119446d2e75
Lisandro <salvareschilisandro0@gmail.com>
2023-07-05 09:16:44 -0300

    fix bug next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
     images: {
+        domains: ["https://cdn.weatherapi.com"],
         remotePatterns: [
             {
                 protocol: 'https',
