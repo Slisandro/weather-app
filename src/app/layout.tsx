edd0524e27556dda8144ab3eedb36bee4801121c
Lisandro <salvareschilisandro0@gmail.com>
2023-10-06 22:56:21 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
